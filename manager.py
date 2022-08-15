0993ddf78cda5553683f777ef70378d8e14dd505
firejoke <38064902+firejoke@users.noreply.github.com>
2022-08-15 20:05:32 +0800

    捕获子进程运行异常并记录到日志

--- manager.py
+++ manager.py
@@ -4,15 +4,27 @@
 import signal
 import sys
 from collections import OrderedDict
+from functools import wraps
 from importlib import import_module
 from multiprocessing import Process
 
 import os
 import time
 
-from public_def import (
-    CONF, common_text, flush_conf, load_conf, root_path, logger,
-)
+from public_def import configuration_path, CONF, flush_conf, load_conf, logger
+
+
+def run_module(func):
+    @wraps(func)
+    def wrapped():
+        try:
+            return func()
+        except Exception as e:
+            logger.error(
+                "%s exception:\n%s\n%s" %
+                (func.__module__, e.__repr__(), e.__str__())
+            )
+    return wrapped
 
 
 if __name__ == '__main__':
@@ -20,23 +32,24 @@ if __name__ == '__main__':
     main_pid = os.getpid()
     logger.info("main pid: %s" % main_pid)
     try:
-        old_conf_stat = os.stat(os.path.join(root_path, 'keeper.yaml'))
+        old_conf_stat = os.stat(configuration_path)
     except OSError:
-        logger.error('not found config: %s' %
-                     os.path.join(root_path, 'keeper.yaml'))
+        logger.error('not found config: %s' % configuration_path)
         sys.exit(1)
     sub_processes = OrderedDict()
+    # TODO: 一个依赖程序准备好了的标志
+    interval = 20
     default = CONF["keeper"]
-    # append srkv
-    default["procs"].append({
-        "name": "srkv.server",
-        "reload_max": 0,
-        "require": None,
-    })
+    default["procs"].insert(
+        0,
+        {
+            "name": "srkv.server",
+            "reload_max": 0,
+            "requires": None,
+        }
+    )
 
     for p in default["procs"]:
-        # if p["name"] in sub_processes:
-        #     continue
         logger.info("import package: %s" % p)
         try:
             package = import_module(p["name"])
@@ -44,22 +57,29 @@ if __name__ == '__main__':
                 p["reload_max"] = float("+inf")
             else:
                 p["reload_max"] = float(p["reload_max"])
-            p["last_reload"] = time.time()
-            p["reload_number"] = 0
+            if not isinstance(p.get("requires"), list):
+                if p.get("requires"):
+                    logger.warning("require must list")
+                p["requires"] = list()
+            if p["name"] != "srkv.server":
+                p["requires"].insert(0, "srkv.server")
+            _proc = getattr(package, "proc")
             sub_processes[p["name"]] = dict(
                 active=True,
-                target=getattr(package, "proc"),
-                process=Process(target=getattr(package, "proc")),
-                require=p["require"],
-                reload_max=p["reload_max"]
+                target=run_module(_proc),
+                process=Process(target=run_module(_proc)),
+                requires=p["requires"],
+                reload_max=p["reload_max"],
+                last_reload=time.time(),
+                reload_number=0
             )
         except ImportError as e:
             logger.error(e)
 
     def exit_procs(signum=None, frame=None):
         if os.getpid() == main_pid:
+            logger.info("Server kill.\n")
             logger.debug(
-                "Server kill.\n"
                 "signum: {0}\nframe:\n\tf_back:{1}\n\tf_builtins:{2}\n"
                 "\tframe.f_code:{3}\n\tframe.f_exc_traceback:{4}\n"
                 "\tframe.f_exc_type:{5}\n\tframe.f_exc_value:{6}\n"
@@ -84,52 +104,59 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, exit_procs)
     while 1:
         time.sleep(1)
-        if os.path.exists(os.path.join(root_path, 'keeper.yaml')) and \
-                old_conf_stat != os.stat(
-                os.path.join(root_path, 'keeper.yaml')):
+        if os.path.exists(configuration_path) \
+                and old_conf_stat != os.stat(configuration_path):
             logger.warning('keeper.yaml already change')
             __load_message = load_conf()
             if __load_message:
                 logger.error(__load_message)
             else:
                 logger.info("reload configfile")
-            old_conf_stat = os.stat(os.path.join(root_path, 'keeper.yaml'))
+            old_conf_stat = os.stat(configuration_path)
         alive_state = list()
         for k, v in sub_processes.items():
-            if not v["require"] or sub_processes[v["require"]]["process"].is_alive():
+            for _m in v["requires"]:
+                if not sub_processes.get(_m):
+                    logger.error("%s not load, %s will not load" % (_m, k))
+                    break
+                if not sub_processes[_m]["process"].is_alive():
+                    logger.error("%s is not alive, %s will not load" % (_m, k))
+                    break
+            else:
                 if not v["active"]:
                     logger.info("%s.active is False" % k)
                     continue
                 if v["process"].pid:
                     if not v["process"].is_alive():
                         logger.error("\"%s\" exit" % k)
-                        if v["reload_number"] <= v["reload_max"]:
-                            if time.time() - v["last_reload"] > 5 * 60:
-                                v["reload_number"] = 0
-                                v["last_reload"] = time.time()
-                            v["process"].terminate()
-                            logger.warning('"%s" terminate.' % k)
-                            v["process"] = Process(target=v["target"])
-                            logger.warning('"%s" will restart.' % k)
+                        v["process"].terminate()
+                        logger.warning('"%s" terminate.' % k)
+                        v["process"] = Process(target=v["target"])
+                        logger.warning('"%s" will restart.' % k)
+                else:
+                    if v["reload_number"] <= v["reload_max"]:
+                        if time.time() - v["last_reload"] > 5 * 60:
+                            v["reload_number"] = 0
+                            v["last_reload"] = time.time()
+                        logger.info("\"%s\" start" % k)
+                        try:
                             v["process"].daemon = True
                             v["process"].start()
                             v["reload_number"] += 1
-                        else:
-                            logger.warning(
-                                '"%s": The maximum number of reloads exceeded'
-                                % v["name"]
-                            )
-                else:
-                    logger.info("\"%s\" start" % k)
-                    try:
-                        v["process"].daemon = True
-                        v["process"].start()
-                        logger.info("%s pid: %s" % (k, v["process"].pid))
-                    except Exception as e:
-                        logger.error(common_text(e.__repr__()))
+                            logger.info("%s pid: %s" % (k, v["process"].pid))
+                            if k == "srkv.server":
+                                logger.info("Intermittent %s seconds" % interval)
+                                time.sleep(interval)
+                        except Exception as e:
+                            logger.error(e)
+                    else:
+                        logger.warning(
+                            '"%s": The maximum number of reloads exceeded' % k
+                        )
+                        sub_processes.pop(k)
                 alive_state.append(v["process"].is_alive())
         if not any(alive_state):
             logger.error("all sub process exit")
             break
     logger.info("Service exit")
-    sys.exit(1)
+    sys.exit(signal.SIGINT)
