7104b385530c01f18917e1e94ce0c38ecde62caa
firejoke <38064902+firejoke@users.noreply.github.com>
2022-10-13 21:06:15 +0800

    增加uuid字段用作节点比对，更快更小

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 # Author      : ShiFan
 # Created Date: 2022/5/12 16:41
 from sqlalchemy import (
-    Boolean, Column, DateTime, Integer, PickleType, String, Table, func, text,
+    Boolean, Column, DateTime, Integer, PickleType, String, Table, func
 )
 from sqlalchemy.orm import mapper
 
@@ -14,10 +14,8 @@ _format = '%Y-%m-%d %H:%M:%f'
 
 repository = Table(
     "repository", ModelBase.metadata,
-    Column("id", Integer, primary_key=True),
-    Column("key", String(256), unique=True, index=True),
+    Column("key", String(256), primary_key=True, unique=True, nullable=False),
     Column("value", PickleType),
-    Column("deleted", Boolean),
     Column(
         "create_time", DateTime,
         server_default=func.strftime(_format, 'now', 'localtime', type_=DateTime),
@@ -27,29 +25,28 @@ repository = Table(
         server_default=func.strftime(_format, 'now', 'localtime', type_=DateTime),
         onupdate=func.strftime(_format, 'now', 'localtime', type_=DateTime)
     ),
-    sqlite_autoincrement=True
 )
 
 
 class Repository(object):
-    def __init__(self, key, value, deleted):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.deleted = deleted
 
 
 mapper(Repository, repository)
 
 
 transaction = Table(
-    "transaction", ModelBase.metadata,
+    "srkv_transaction", ModelBase.metadata,
     Column("id", Integer, primary_key=True),
-    Column("action", String(128)),
-    Column("key", String(128)),
-    Column("value", PickleType),
-    Column("roll_action", String(128), nullable=True),
+    Column("action", String(128), nullable=False),
+    Column("key", String(128), nullable=False),
+    Column("value", PickleType, nullable=True),
+    Column("roll_action", String(128), nullable=False),
     Column("roll_value", PickleType, nullable=True),
     Column("state", String(16), default="ready"),
+    Column("uuid", String(16), unique=True, nullable=False),
     Column(
         "create_time", DateTime,
         server_default=func.strftime(_format, 'now', 'localtime', type_=DateTime),
@@ -59,13 +56,15 @@ transaction = Table(
         server_default=func.strftime(_format, 'now', 'localtime', type_=DateTime),
         onupdate=func.strftime(_format, 'now', 'localtime', type_=DateTime)
     ),
-    # sqlite_autoincrement=True
 )
 
 
 class Transaction(object):
-    def __init__(self, id, action, key, value, roll_action, roll_value, state):
+    def __init__(
+        self, id, uuid, action, key, value, roll_action, roll_value, state
+    ):
         self.id = id
+        self.uuid = uuid
         self.action = action
         self.key = key
         self.value = value
@@ -80,4 +79,4 @@ mapper(Transaction, transaction)
 class Nodes(ModelBase):
     __tablename__ = "nodes"
     sys_uid = Column(String(512), primary_key=True)
-    ipaddresses = Column(String(255))
+    ipaddresses = Column(String(255), nullable=False)
